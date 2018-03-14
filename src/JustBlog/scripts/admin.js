acc71b317c0f62688c39d45dc228a38f8fd63ea8
Luthfi Azzammi <luthfia911@gmail.com>
2018-03-14 08:03:18 +0700

    Tidak tahu sampai mana

--- src/JustBlog/scripts/admin.js
+++ src/JustBlog/scripts/admin.js
@@ -1,3 +1,25 @@
 ﻿$(function () {
-    $("#tabs").tabs();
+    $("#tabs").tabs({
+        show: function (event, ui) {
+            if (!ui.tab.isLoaded)
+            {
+                switch (ui.index) {
+                    case 0:
+                        // call function to create grid for managing posts
+                        // from "#tablePosts" and "#pagerPosts"
+                        break;
+                    case 1:
+                        // call function to create grid for managing posts
+                        // from "#tableCats" and "#pagerCats"
+                        break;
+                    case 2:
+                        // call function to create grid for managing posts
+                        // from "#tableTags" and "#pagerTags";
+                        break;
+                };
+
+                ui.tab.isLoaded = true;
+            }
+        }
+    });
 });
